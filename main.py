5c6954cf588d62c3d26be4241e74f660a7635382
jhonatanino <111208351+jhonatanino@users.noreply.github.com>
2022-09-12 21:44:03 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+#Esto es un comentario de linea 
+
+"""
+Esto es un comentario de 
+bloque
+"""
+
+mensaje = "Hola mundo"
+
+print(mensaje)
+
+nombre = "Jhonatan"
+Apellido = "Nino"
+Ciudad = "Soacha"
+Celular = 3057626658
+
+print (f"hola {nombre} con {Apellido} de la {Ciudad} y numero {Celular}")
+
+nombre = input("digita tu nombre: ")
+print (nombre)
+
+
+
+
+# Leer datos desde teclado
+
+
